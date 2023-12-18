225e3a1a91af7e27bafbff59e17d35985c27be38
IgorOkhman <ioi@cc-interactive.com>
2023-12-18 19:22:10 +0200

    create meal styling

--- public/js/MealCreator.js
+++ public/js/MealCreator.js
@@ -2,41 +2,38 @@ function mealCreatorUpdateContent(element) {
 	//clear element content
 	element.innerHTML = '';
 	let content = '' +
-		'<div class="pageContent mealPage">' +
-		'	<div class = "pageTitle">' +
-		'		<div class="titleContent">Meal Creator</div>' +
-		'		<div class="buttons">' +
-		'			<div class="button" onclick="mealCreatorAddMeal(\'' + element.id + '\');">+</div>' +
-		'		</div>' +
+		'<div class="pageContent mealPage meal-tracker-card-container">' +
+		'	<div class = "title-container">' +
+		'		<div class="title">Meal Creator</div>' +
+		'			<button class="addMealButton" onclick="mealCreatorAddMeal(\'' + element.id + '\');">Add Meal</button>' +
 		'	</div>';
 
-	content += '<div class="itemBlock itemTableInfo">' +
-		'	<div class="itemRow itemHeaderRow">' +
-		'		<div class="itemCell itemHeader">Meal Name</div>' +
-		'		<div class="itemCell itemHeader">Total Kcal pr. 100g</div>' +
-		'		<div class="itemCell itemHeader">Added on</div>' +
-		'		<div class="itemCell itemHeader"># Ingredients</div>' +
-		'		<div class="itemCell itemHeader">Times Eaten</div>' +
-		'		<div class="itemCell itemHeader">Actions</div>' +
-		'	</div>' +
+	content += 
+		'	<div class="itemRow itemHeaderRow meal-header">' +
+		'		<div class="itemCell itemHeader source" style="width: 20%">Meal Name</div>' +
+		'		<div class="itemCell itemHeader source" style="width: 15%">Total Kcal pr. 100g</div>' +
+		'		<div class="itemCell itemHeader source" style="width: 12%">Added on</div>' +
+		'		<div class="itemCell itemHeader source" style="width: 12%"># Ingredients</div>' +
+		'		<div class="itemCell itemHeader source" style="width: 12%">Times Eaten</div>' +
+		'		<div class="itemCell itemHeader source" style="width: 12%; justify-content: end;">Actions</div>' +
 		'';
+	
+	content += '</div>';
 	for (const key in appData.meal) {
 		const meal = appData.meal[key];
 		content += '' +
-			'	<div class="itemRow itemId' + meal.id + '">' +
-			'		<div class="itemCell editable">' + meal.name + '</div>' +
-			'		<div class="itemCell">' + meal.energy + '</div>' +
-			'		<div class="itemCell">' + meal.createDate.toLocaleDateString() + '</div>' +
-			'		<div class="itemCell">' + meal.ingredients.length + '</div>' +
-			'		<div class="itemCell"> Meal Tracker Integration </div>' +
-			'		<div class="itemCell buttons">' +
-			'			<div class="button buttonEdit" >edit</div>' +
+			'	<div class="itemRow meal itemId' + meal.id + '">' +
+			'		<div class="itemCell editable" style="width: 20%">' + meal.name + '</div>' +
+			'		<div class="itemCell" style="width: 15%">' + meal.energy + '</div>' +
+			'		<div class="itemCell" style="width: 12%">' + meal.createDate.toLocaleDateString() + '</div>' +
+			'		<div class="itemCell" style="width: 12%">' + meal.ingredients.length + '</div>' +
+			'		<div class="itemCell" style="width: 12%"> Meal Tracker Integration </div>' +
+			'		<div class="itemCell buttons" style="width: 12%; justify-content: end;">' +
+			'			<div class="button buttonEdit" >✏️</div>' +
 			'		</div>' +
 			'	</div>' +
 			'';
 	}
-	content += '</div>';
-
 	content += '</div>';
 	element.innerHTML += content;
 }
