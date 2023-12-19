403c913b3c0ec9f3bb7857d37a119c5f5da4cf91
IgorOkhman <ioi@cc-interactive.com>
2023-12-19 17:31:38 +0200

    Finished nutri report

--- public/js/NutriReport.js
+++ public/js/NutriReport.js
@@ -1,21 +1,28 @@
 function aggregateNutritionData() {
     const aggregatedData = {};
   
-    Object.values(appData.mealTrecker).forEach(meal => {
-      const date = meal.addedOn;
-      if (!aggregatedData[date]) {
-        aggregatedData[date] = { meals: 0, water: 0, kcal: 0, protein: 0, fat: 0, fiber: 0 };
+    Object.values(appData.mealTrecker).forEach(mealTreckerItem => {
+      const date = mealTreckerItem.addedOn;
+      const mealDetails = appData.meal[mealTreckerItem.mealId];
+      if (mealDetails) {
+        const weightMultiplier = parseFloat(mealTreckerItem.weight) / 100;
+  
+        if (!aggregatedData[date]) {
+          aggregatedData[date] = { meals: 0, water: 0, kcal: 0, protein: 0, fat: 0, fiber: 0 };
+        }
+  
+        aggregatedData[date].meals += 1;
+        aggregatedData[date].water += (parseFloat(mealDetails.water) || 0) * weightMultiplier;
+        aggregatedData[date].kcal += parseFloat(mealTreckerItem.energy) || 0; // Assuming energy is already in kcal and does not need weight multiplier
+        aggregatedData[date].protein += (parseFloat(mealDetails.protein) || 0) * weightMultiplier;
+        aggregatedData[date].fat += (parseFloat(mealDetails.fat) || 0) * weightMultiplier;
+        aggregatedData[date].fiber += (parseFloat(mealDetails.fiber) || 0) * weightMultiplier;
       }
-      aggregatedData[date].meals += 1;
-      aggregatedData[date].water += parseFloat(meal.water) || 0;
-      aggregatedData[date].kcal += parseFloat(meal.energy) || 0;
-      aggregatedData[date].protein += parseFloat(meal.protein) || 0;
-      aggregatedData[date].fat += parseFloat(meal.fat) || 0;
-      aggregatedData[date].fiber += parseFloat(meal.fiber) || 0;
     });
   
     return aggregatedData;
   }
+  
   
   function createNutritionCard(date, data) {
     const card = document.createElement("div");
@@ -23,12 +30,12 @@ function aggregateNutritionData() {
   
     card.innerHTML = `
       <div style="width: 20%">${date}</div>
-      <div style="width: 12%">${data.meals}</div>
-      <div style="width: 12%">${data.water.toFixed(2)}</div>
-      <div style="width: 12%">${data.kcal.toFixed(2)}</div>
-      <div style="width: 12%">${data.protein.toFixed(2)}</div>
-      <div style="width: 12%">${data.fat.toFixed(2)}</div>
-      <div style="width: 12%; text-align: end;">${data.fiber.toFixed(2)}</div>
+      <div style="width: 12%">${data.meals} Meals</div>
+      <div style="width: 12%">${data.water.toFixed(2)}g</div>
+      <div style="width: 12%">${data.kcal.toFixed(2)} kcal</div>
+      <div style="width: 12%">${data.protein.toFixed(2)}g</div>
+      <div style="width: 12%">${data.fat.toFixed(2)}g</div>
+      <div style="width: 12%; text-align: end;">${data.fiber.toFixed(2)}g</div>
     `;
   
     return card;
